157dd41e586263e9b2c6ab67aca5e16443a3234c
Achilovraf <Achilovraf@gmail.com>
2025-11-21 14:48:21 +0500

    Fix base path - use lowercase

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/Albadent/",
+  base: "/albadent/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
